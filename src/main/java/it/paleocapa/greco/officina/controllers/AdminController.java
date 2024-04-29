bd251cf66bf640b52692ca37e5d3f6ca58351be6
Greco Mattia Gaetano <Greco.19214>
2024-04-29 13:31:36 +0200

    kanban functionalities extended to admin (shared code)

--- src/main/java/it/paleocapa/greco/officina/controllers/AdminController.java
+++ src/main/java/it/paleocapa/greco/officina/controllers/AdminController.java
@@ -6,24 +6,47 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
-import it.paleocapa.greco.officina.model.Shop;
+import it.paleocapa.greco.officina.controllers_shared_interfaces.implementations.KanbanInterfaceImplementation;
+import it.paleocapa.greco.officina.controllers_shared_interfaces.interfaces.KanbanInterface;
+import it.paleocapa.greco.officina.model.Ordine;
 import it.paleocapa.greco.officina.user_details.AdminDetails;
 import it.paleocapa.greco.officina.utilities.Utilities;
 
 @Controller
 @RequestMapping("/admin")
-public class AdminController {
+public class AdminController implements KanbanInterface {
 
     @RequestMapping(value="/home", method=RequestMethod.GET)
-    public String homeAdmin(Model model) {
-        AdminDetails adminDetails = AdminUtils.getAuthenticatedAdminDetails();
-        String getShopsURI = "/api/officina/find_by_admin?id="+adminDetails.getUser().getId_admin();
-        Shop[] shops = Utilities.getFromAPI(getShopsURI, Shop[].class);
-        model.addAttribute("principal", adminDetails);
-        model.addAttribute("shops", shops);
-        return "admin/home";
+    public RedirectView homeAdmin(Model model) {
+        return new RedirectView("/admin/kanban?pos_kanban=0");
+    }
+
+    @RequestMapping(value="/kanban", method=RequestMethod.GET)
+    public String vistaKanbanGet(@RequestParam("pos_kanban") String pos_kanban, Model model) {
+        return KanbanInterfaceImplementation.vistaKanbanGet(pos_kanban, model, AdminDetails.class);
+    }
+
+    @RequestMapping(value="/aggiungi_ordine", method=RequestMethod.GET)
+    public String aggiungiOrdineGet(Model model) {
+        return KanbanInterfaceImplementation.aggiungiOrdineGet(model, AdminDetails.class);
+    }
+
+    @RequestMapping(value="/aggiungi_ordine", method=RequestMethod.POST)
+    public RedirectView aggiungiOrdinePost(Model model, @ModelAttribute("ordine") Ordine ordine) {
+        return KanbanInterfaceImplementation.aggiungiOrdinePost(model, ordine, AdminDetails.class);
+    }
+
+    @RequestMapping(value="/modifica_ordine", method=RequestMethod.GET)
+    public String modificaOrdineGet(Model model, @RequestParam("id_ordine") String id_ordine) {
+        return KanbanInterfaceImplementation.modificaOrdineGet(model, id_ordine, AdminDetails.class);
+    }
+
+    @RequestMapping(value="/elimina_ordine", method=RequestMethod.POST)
+    public RedirectView eliminaOrdine(Model model, @RequestParam("id_ordine") String id_ordine) {
+        return KanbanInterfaceImplementation.eliminaOrdine(model, id_ordine, AdminDetails.class);
     }
 
     @RequestMapping(value="visualizza_officine", method=RequestMethod.GET)
--- src/main/java/it/paleocapa/greco/officina/controllers/DipendenteController.java
+++ src/main/java/it/paleocapa/greco/officina/controllers/DipendenteController.java
@@ -9,15 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
-import it.paleocapa.greco.officina.model.Kanban;
+import it.paleocapa.greco.officina.controllers_shared_interfaces.implementations.KanbanInterfaceImplementation;
+import it.paleocapa.greco.officina.controllers_shared_interfaces.interfaces.KanbanInterface;
 import it.paleocapa.greco.officina.model.Ordine;
 import it.paleocapa.greco.officina.user_details.DipendenteDetails;
-import it.paleocapa.greco.officina.utilities.OrdineFields;
 import it.paleocapa.greco.officina.utilities.Utilities;
 
 @Controller
 @RequestMapping("/dipendente")
-public class DipendenteController {
+public class DipendenteController implements KanbanInterface {
 
     @RequestMapping(value="/home", method=RequestMethod.GET)
     public RedirectView homeDipendente(Model model, RedirectAttributes attributes) {
@@ -25,54 +25,28 @@ public class DipendenteController {
     }
 
     @RequestMapping(value="/kanban", method=RequestMethod.GET)
-    public String vistaKanban(@RequestParam("pos_kanban") String pos_kanban, Model model) {
-        int id_shop = (Utilities.getAuthenticatedUser(DipendenteDetails.class).getUser().getShop().getId_shop()).intValue();
-        String getKanbansFromShopURI = "/api/officina/get_kanbans?shop_id="+id_shop;
-        String getOrdersFromShopURI = "/api/officina/get_ordini_kanban?shop_id="+id_shop+"&pos_kanban="+pos_kanban;
-        Kanban[] kanbans = Utilities.getFromAPI(getKanbansFromShopURI, Kanban[].class);
-        Ordine[] ordini = Utilities.getFromAPI(getOrdersFromShopURI, Ordine[].class);
-        model = Utilities.putPrincipal(model, DipendenteDetails.class);
-        model.addAttribute("kanbans", kanbans);
-        model.addAttribute("ordini", ordini);
-        model.addAttribute("show_fields", OrdineFields.show_fields);
-        return "dipendente/home";
+    public String vistaKanbanGet(@RequestParam("pos_kanban") String pos_kanban, Model model) {
+        return KanbanInterfaceImplementation.vistaKanbanGet(pos_kanban, model, DipendenteDetails.class);
     }
 
     @RequestMapping(value="/aggiungi_ordine", method=RequestMethod.GET)
     public String aggiungiOrdineGet(Model model) {
-        int id_shop = (Utilities.getAuthenticatedUser(DipendenteDetails.class).getUser().getShop().getId_shop()).intValue();
-        //DipendenteDetails dipendenteDetails = ;
-        String getKanbansFromShopURI = "/api/officina/get_kanbans?shop_id="+id_shop;
-        Kanban[] kanbans = Utilities.getFromAPI(getKanbansFromShopURI, Kanban[].class);
-        model = Utilities.putPrincipal(model, DipendenteDetails.class);
-        model = Utilities.putOrdineInputFields(model, Utilities.getAuthenticatedUser(DipendenteDetails.class).getUser().getShop().getId_shop());
-        model.addAttribute("kanbans", kanbans);
-        return "dipendente/aggiungi_ordine";
+        return KanbanInterfaceImplementation.aggiungiOrdineGet(model, DipendenteDetails.class);
     }
 
     @RequestMapping(value="/aggiungi_ordine", method=RequestMethod.POST)
     public RedirectView aggiungiOrdinePost(Model model, @ModelAttribute("ordine") Ordine ordine) {
-        ordine = Utilities.formatInputtedOrder(ordine);
-        if (ordine == null)
-            return new RedirectView("/dipendente/aggiungi_ordine");
-        Utilities.postToAPI("/api/ordine/create", ordine, Ordine.class);
-        return new RedirectView("/dipendente/kanban?pos_kanban="+ordine.getKanban().getPosizione());
+        return KanbanInterfaceImplementation.aggiungiOrdinePost(model, ordine, DipendenteDetails.class);
     }
 
     @RequestMapping(value="/modifica_ordine", method=RequestMethod.GET)
     public String modificaOrdineGet(Model model, @RequestParam("id_ordine") String id_ordine) {
-        Ordine ordine = Utilities.getFromAPI("/api/ordine/get?id="+id_ordine, Ordine.class);
-        model = Utilities.putPrincipal(model, DipendenteDetails.class);
-        model = Utilities.putOrdineInputFields(model, Utilities.getAuthenticatedUser(DipendenteDetails.class).getUser().getShop().getId_shop());
-        model.addAttribute("ordine", ordine);
-        model.addAttribute("is_modify", true);
-        return "dipendente/aggiungi_ordine";
+        return KanbanInterfaceImplementation.modificaOrdineGet(model, id_ordine, DipendenteDetails.class);
     }
 
     @RequestMapping(value="/elimina_ordine", method=RequestMethod.POST)
     public RedirectView eliminaOrdine(Model model, @RequestParam("id_ordine") String id_ordine) {
-        Utilities.postToAPI("/api/ordine/delete?id="+id_ordine, null, Void.class);
-        return new RedirectView("/dipendente/home");
+        return KanbanInterfaceImplementation.eliminaOrdine(model, id_ordine, DipendenteDetails.class);
     }
 
     @RequestMapping(value="/aggiorna_dati", method=RequestMethod.POST)
